8106751c4a9b77969225ae1ee5f85d4b1a7b4b28
feliciajayasaputra <feljayasaputra@gmail.com>
2025-02-21 10:39:44 -0700

    Update Methods

--- backend/management/models.py
+++ backend/management/models.py
@@ -4,4 +4,21 @@ from django.db import models
 
 class InventoryItem(models.Model):
     item_name = models.TextField()
-    count = models.IntegerField()
+    stock_count = models.IntegerField()
+    minimum_count = models.IntegerField()
+    new_item = models.TextField()
+    last_update = models.TextField()
+    description = models.TextField()
+    category_num = models.IntegerField()
+    buy_price = models.IntegerField()
+    sell_price = models.IntegerField()
+    location = models.TextField()
+    code_num = models.IntegerField()
+    weight = models.IntegerField()
+    sold_out = models.TextField()
+    donated
+    purchased
+    
+
+
+
