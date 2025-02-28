b2ba3a806ad601b3c2e848199ace3d7812c3abf4
itsryan1234 <rjohnson924@outlook.com>
2025-02-28 10:22:44 -0700

    user registeration

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from management.views import TestView
+from django.urls import path, include
 
 urlpatterns = [
+    path('register/', include('management.urls')),
     path('admin/', admin.site.urls),
-    path('test/', TestView.as_view())
 ]
--- backend/management/views.py
+++ backend/management/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 # Create your views here.
 
-class TestView(APIView):
-
-    def get(self, request):
-        return Response({"hello" : "hello"})
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            #TODO: Add Jackson's login code
+            messages.success(request, ("Registration Success!"))
+    else:
+        form = UserCreationForm()
+    return render(request, 'authenticate/register.html', {'form':form})
