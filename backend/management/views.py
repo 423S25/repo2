8ee2260d4af63c0434827061d9324b0f025cb680
Joe Thorkildson <joe@joes-mbp.msu.montana.edu>
2025-03-12 10:26:26 -0600

    update views file

--- backend/management/views.py
+++ backend/management/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-# from rest_framework.views import APIView
+from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -7,7 +7,6 @@ from .models import InventoryItem
 from .serializer import ItemSerializer
 
 # Create your views here.
-
 
 @api_view(['GET'])
 def get_items(request):
@@ -51,3 +50,65 @@ def update_status(pk):
     elif item.stock_count / item.base_count < 0.25:
         return "very low"
 '''
+
+'''
+class InventoryManagementView(APIView): 
+    def post(self, request):
+        count = request.get("count")
+        pk = request.get("pk")
+        item = InventoryItem.object.filter(pk=pk)
+        item.count = count 
+        item.update()
+
+    def delete(self, request):
+        pk = request.get("pk")
+        item = InventoryItem.object.filter(pk=pk)
+        item.delete()
+
+    def create(self, request):
+        item_name = request.POST.get("item_name")
+        count = request.POST.get("count")
+        item = InventoryItem.objects.create(item_name=item_name, count=count)
+
+    def search(self, request):
+        pk = request.get("pk")
+        search_item = InventoryItem.object.filter(pk=pk)
+        item_info = [
+            {"pk": item.pk, "item_name": item.item_name, "count": item.stock_count, "description": item.description, "price": item.cost}
+            for item in search_item
+        ]
+        return Response(item_info)
+    
+    #def item_status(self.request):
+        #get the stock count
+        #if the stock count is under 20, label it as low need more stock
+        #if the stock count is above 20, label it as good amount stock.
+        #return stock_count
+
+    def get(self, request):
+        pk = request.query_params.get("pk")
+        action = request.query_params.get("action")
+        if action == "search":
+            return self.all_item(request)
+        if action == "search":
+            return self.all_item(request)
+        try:
+            item = InventoryItem.objects.filter(pk=pk).first()
+        except Exception:
+            print("item not found")
+            return Response({})
+        return Response({"item_name" : item.item_name})
+
+
+class InventoryManagementListView(APIView):
+    def get(self, request):
+        items = InventoryItem.objects.all()
+        serialized_data = ItemSerializer(items, many=True).data
+        return Response(serialized_data)
+       
+'''
+class TestView(APIView):
+    items = InventoryItem.objects.all()
+
+    def get(self, request):
+        return Response({"hello" : "hello"})
