d622a0485852e92b4324954a49dfa0a5054ef592
Joe Thorkildson <joe@joes-mbp.msu.montana.edu>
2025-04-09 10:25:48 -0600

    make changes to model fields

--- backend/management/models.py
+++ backend/management/models.py
@@ -5,13 +5,15 @@ from simple_history.models import HistoricalRecords
 
 class InventoryItem(models.Model):
     item_name = models.TextField(default="")
-    stock_count = models.IntegerField(default=0)
-    base_count = models.IntegerField(default=0)
+    stock_count = models.IntegerField(default=0) # number of items in inventory
+    base_count = models.IntegerField(default=0) # number of minimum individual items
+    bulk_count = models.IntegerField(default=0) # number of bulk cases
+    amount_in_bulk = models.IntegerField(default=0) # number of items in bulk case
     brand = models.TextField(default="")
     item_category = models.TextField(default="")
-    cost = models.IntegerField(default=0)
-    location = models.TextField(default="")
+    individual_cost = models.DecimalField(default=0) # cost of indiviudal items
+    bulk_cost = models.DecimalField(default=0) # cost of a bulk case
     donated = models.BooleanField(default=False)
+    is_bulk = models.BooleanField(default=False)
     status = models.TextField(default="")
     history = HistoricalRecords()
-
