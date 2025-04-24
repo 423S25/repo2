9efdf5b01f0053e63603eac6ffcb00b839842768
itsryan1234 <rjohnson924@outlook.com>
2025-04-23 18:16:02 -0600

    another try

--- backend/management/urls.py
+++ backend/management/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('inventory/', get_items, name='inventory-list'),
     path('inventory/create/', create_item, name='inventory-create'),
     path('inventory/<int:pk>/', modify_item, name='inventory-detail'),
-    path('register/', register_user, name='register'),
     path('inventory/count_history/<int:pk>', get_item_quantity_changes, name='get_item_quantity_changes'),
     path('inventory/history/<int:pk>', get_item_history, name='get_item_history'),
     path("inventory/csv/", DownloadCSV.as_view()),
